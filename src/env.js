9ba7502e97907b2f14eaaa84bbce5fe10d549fb7
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-07-03 00:47:02 -0400

    feature(env): add google environment variables for authentication

--- src/env.js
+++ src/env.js
@@ -22,10 +22,11 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url()
     ),
-    PASSWORD_HASH_ROUNDS:
-      z.coerce.number().min(10),
     UPLOADTHING_SECRET: z.string(),
     UPLOADTHING_APP_ID: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+
   },
 
   /**
@@ -46,9 +47,10 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    PASSWORD_HASH_ROUNDS: process.env.PASSWORD_HASH_ROUNDS,
     UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
     UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
+    GOOGLE_CLIENT_ID:  process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET:  process.env.GOOGLE_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
