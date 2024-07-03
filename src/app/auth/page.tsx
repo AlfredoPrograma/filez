45230386ff6745a61984d849aab95fb629ce939f
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-07-03 06:55:14 -0400

    feature(auth): add guard to avoid authenticated users to access to auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
 import { AuthenticationMethods } from './components/authentication-methods';
+import { getServerAuthSession } from '~/server/auth';
+import { redirect } from 'next/navigation';
 
 export default async function AuthPage() {
+  const session = await getServerAuthSession();
+
+  if (session) {
+    redirect('/dashboard');
+  }
+
   return (
     <main className='flex h-screen items-center justify-center bg-gray-100'>
       <Card>
